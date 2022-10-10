7fa925bdcf4810e7f1c65eae7a1917f4e831cd86
AsafEitani <asaf.eitani@aquasec.com>
2022-10-10 15:33:54 +0300

    upgrade project and add first admin pages

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,31 @@
 var express = require('express');
 var router = express.Router();
 
+const products_test = [{
+  ProductName: "Guitar 3000",
+  ProductColor: "red",
+  ProductPrice: "123",
+  ProductID: "1",
+  ProductImage: "img"
+}]
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/admin', function (req, res) {
+    res.render('admin', { title: 'Admin' });
+});
+
+router.get('/admin_login', function (req, res) {
+    res.render('admin_login', { title: 'Admin Login' });
+});
+
+router.get('/admin_products', function (req, res) {
+  res.render('admin_products', {
+    title: 'Admin Products', products_test: products_test
+  });
 });
 
 module.exports = router;
